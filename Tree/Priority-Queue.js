3251fc09ca7123b8ec479f1a3437c13998234fdd
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2020-04-12 16:24:36 -0400

    add binary heap and priority queue

---
+++ Tree/Priority-Queue.js
@@ -0,0 +1,95 @@
+// **************************************************
+//       Priority Queue
+// **************************************************
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIdx = Math.floor((index - 1) / 2);
+      const parrent = this.values[parentIdx];
+      if (parrent.priority <= element.priority) break;
+      // Bubble up
+      this.values[parentIdx] = element;
+      this.values[index] = parrent;
+      index = parentIdx;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      // Sift down
+      this.siftDown();
+    }
+    return min;
+  }
+
+  siftDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * index + 1;
+      let rightChildIdx = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element; // element never changes
+      index = swap;
+    }
+  }
+}
+
+const ER = new PriorityQueue();
+ER.enqueue('common cold', 5);
+ER.enqueue('gunshot wound', 1);
+ER.enqueue('high fever', 4);
+ER.enqueue('broken arm', 2);
+ER.enqueue('glass in foot', 3);
+console.log(ER.dequeue());
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
